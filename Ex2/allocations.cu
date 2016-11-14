548e5d75153d1ec6b76b9013c635922a9fb923ca
Argel Ramírez Reyes <argel.ramirez@gmail.com>
2016-11-14 23:06:53 +0100

    Ex 1 of CUDA done

--- Ex2/allocations.cu
+++ Ex2/allocations.cu
@@ -8,25 +8,19 @@ int main (int argc, char **argv){
   int* b_d;
   int* c_h;
   dim3 dG(1);
-  dim3 dB(1);
-
-  printf("Checkpoint -2\n");
+  dim3 dB(16,8,1);
   i = atoi(argv[1]);
-  printf("Checkpoint -1\n");
   a_h = (int*) malloc(i*sizeof(int));
-  printf("Checkpoint 0\n");
   cudaMalloc((void**)&b_d,i*sizeof(int));
   c_h = (int*) malloc(i*sizeof(int));
-  printf("Checkpoint 1\n");
   for(j=0;j<i;j++){
     *(a_h+j) = j;
     printf("The value at a+%d is: %d \n",j,*(a_h+j));
   }
   cudaMemcpy(b_d,a_h,i*sizeof(int),cudaMemcpyHostToDevice);
-  increase<<< 1, 1>>>(b_d);
+  inc_gpu<<< dG, dB>>>(b_d,i);
   cudaMemcpy(c_h,b_d,i*sizeof(int),cudaMemcpyDeviceToHost);
   for(j=0;j<i;j++)
     printf("The value at c+%d is: %d \n",j,*(c_h+j));
-  printf("Checkpoint 3\n"); 
   return 0;
 }
--- Ex2/allocations_kernel.cu
+++ Ex2/allocations_kernel.cu
@@ -7,6 +7,11 @@ __global__ void increase(int* src){
   src[0]++;  
 }
 
+__global__ void inc_gpu(int* src, int n){
+  int tid = threadIdx.y*blockDim.x + threadIdx.x;
+  if(tid<n)
+    src[tid]++;
+}
 
 
 
